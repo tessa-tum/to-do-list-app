892177ce222c7e90332b97e3367063253d973127
tessatum <125835305+tessatumbraegel@users.noreply.github.com>
2023-04-05 19:19:55 +0200

    Add scripts (jQuery)

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+function newItem(){
+  
+  //Add new item to list
+  let li = $('<li></li>');
+  let inputValue = $("#input").val();
+  li.append(inputValue);
+  
+  if (inputValue === '') {
+    alert("Please write something!");
+  } else {
+    $('#list').append(li);
+  }
+  
+  //Cross out an item from the list 
+  li.on("dblclick", function(){
+    li.toggleClass("strike");
+  });
+  
+  //Add delete button "X"
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append(document.createTextNode('X'));
+  li.append(crossOutButton);
+  
+  crossOutButton.on("click", function(){
+    li.addClass("delete");
+  });
+  
+  // Reorder items
+  $('#list').sortable();
+
+}
